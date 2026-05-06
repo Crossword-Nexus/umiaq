a7f74f7fccec7d511925cfc61f7676e76a96abd8
horwitz <horwitz@users.noreply.github.com>
2026-05-06 06:34:49 +0000

    deploy: 5ad650a1d114f5b564cd9e6b4e32ccf433ad6b8f

--- staging/docs/trait.impl/core/marker/trait.Copy.js
+++ staging/docs/trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["umiaq",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"umiaq/constraints/struct.Bounds.html\" title=\"struct umiaq::constraints::Bounds\">Bounds</a>",0]]]]);
+    const implementors = Object.fromEntries([["umiaq",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"umiaq/constraints/struct.Bounds.html\" title=\"struct umiaq::constraints::Bounds\">Bounds</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
